2ef7ac8d04e3d310c0ceba97371b093b3b26a2e1
William <william.guimont-martin.1@ulaval.ca>
2019-02-05 22:44:49 -0500

    Refactor

---
+++ example.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from dbscan import DBSCAN
+
+if __name__ == '__main__':
+    epsilon = 0.5
+    min_pts = 2
+    points = np.array(
+        [(np.cos(x), np.sin(x)) for x in np.linspace(0, 2 * np.pi, 100)] +
+        [(2 * np.cos(x), 2 * np.sin(x)) for x in np.linspace(0, 2 * np.pi, 100)] +
+        [(3 * np.cos(x), 3 * np.sin(x))
+         for x in np.linspace(0, 2 * np.pi, 100)]
+    )
+
+    def euclidean_distance_2d(x, y):
+        return (x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2
+
+    clusterer = DBSCAN(euclidean_distance_2d, epsilon, min_pts)
+    clusters = clusterer.cluster(points)
+
+    for points in clusters.values():
+        pt_cluster = np.array(points)
+        plt.scatter(pt_cluster[:, 0], pt_cluster[:, 1])
+    plt.show()
